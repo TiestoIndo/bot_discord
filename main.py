cf48896c9c978a3930a45e1fc5eb6843b3ee6ba1
TiestoIndo <111359095+TiestoIndo@users.noreply.github.com>
2023-08-15 18:32:57 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,32 @@
-# This example requires the 'message_content' privileged intent to function.
-
 import discord
+from discord.ext import commands
+import os
 import random
-import asyncio
-
-
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print(f'Logged in as {self.user} (ID: {self.user.id})')
-        print('------')
-
-    async def on_message(self, message):
-        # we do not want the bot to reply to itself
-        if message.author.id == self.user.id:
-            return
-
-        if message.content.startswith('$guess'):
-            await message.channel.send('Guess a number between 1 and 10.')
-
-            def is_correct(m):
-                return m.author == message.author and m.content.isdigit()
-
-            answer = random.randint(1, 10)
-
-            try:
-                guess = await self.wait_for('message', check=is_correct, timeout=5.0)
-            except asyncio.TimeoutError:
-                return await message.channel.send(f'Sorry, you took too long it was {answer}.')
-
-            if int(guess.content) == answer:
-                await message.channel.send('You are right!')
-            else:
-                await message.channel.send(f'Oops. It is actually {answer}.')
-
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = MyClient(intents=intents)
-client.run('token')
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+# Dan inilah cara Kamu mengganti nama file dari variabel!
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hi! I am a bot {bot.user}!')
+organik = ['daun', 'kotoran hewan', 'kertas', 'sisa makanan']
+anorganik = ['plastik', 'besi', 'kabel', 'emas']
+@bot.command()
+async def pilih(ctx):
+    await ctx.send('Masukkan sampah')
+
+    msg = await bot.wait_for("message")
+
+    if msg.content in organik:
+        await ctx.send("Masukkan dalam organik")
+    elif msg.content in anorganik:
+        await ctx.send("Masukkan dalam anorganik")
+bot.run("token")
